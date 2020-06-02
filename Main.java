0a7cc4d71610cfd8ce8b5222118c884095292afe
Simon Poirier <cpsiat.spoirier@gmail.com>
2020-06-02 15:32:55 +0200

    commit commenaire nul

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main {
   public static void main(final String[] args) {
     System.out.println("This is the way 2!");
 
+    //commentaire dtrop nul
     System.out.println(Math.sqrt(10.0));
   }
 }
