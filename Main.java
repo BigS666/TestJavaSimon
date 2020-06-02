c7e429cb03191b6dc4d1355082badbfdc2a11596
Simon Poirier <cpsiat.spoirier@gmail.com>
2020-06-02 15:30:51 +0200

    commit

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.lang.Math;
+
 class Main {
   public static void main(final String[] args) {
     System.out.println("This is the way 2!");
+
+    System.out.println(Math.sqrt(10.0));
   }
 }
