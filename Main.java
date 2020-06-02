eb6b6d5ccf1e4278f072ed48b4f635e5dc6b35ff
Simon Poirier <cpsiat.spoirier@gmail.com>
2020-06-02 11:26:52 +0200

    v2

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("This is the way!");
+  public static void main(final String[] args) {
+    System.out.println("This is the way 2!");
   }
 }
